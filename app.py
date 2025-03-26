2666c5a23ff99b3613bed453ee8bb0ea636201cd
IsaiZ734 <isaimola@gmail.com>
2025-03-26 15:36:32 +0100

    Done with the div function

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ class Calculator:
 
     def add(self, num1, num2):
         return num1 + num2
+    
+    def substraction(self, num1, num2):
+        return num1 - num2
+    
+    def multiplication(self, num1, num2):
+        return num1 * num2
+    
+    def division(self, num1, num2):
+        return num1 / num2

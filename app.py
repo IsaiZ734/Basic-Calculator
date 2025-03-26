bd44c60c98a1f48a078ba7dce1656a4044c7ef62
IsaiZ734 <isaimola@gmail.com>
2025-03-26 15:42:32 +0100

    division function rename

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ class Calculator:
     def add(self, num1, num2):
         return num1 + num2
     
-    def substraction(self, num1, num2):
+    def sub(self, num1, num2):
         return num1 - num2
     
-    def multiplication(self, num1, num2):
+    def mul(self, num1, num2):
         return num1 * num2
     
-    def division(self, num1, num2):
+    def div(self, num1, num2):
         return num1 / num2
